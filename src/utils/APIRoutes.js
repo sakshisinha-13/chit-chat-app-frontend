1564a8ad50e360cd1383fadc24d11c8d8034a5cf
Atharv7313 <137501939+Atharv7313@users.noreply.github.com>
2025-06-16 21:03:46 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 // export const host="http://localhost:5000";
-export const host="https://chit-chat-app-api-zeta.vercel.app/";
+export const host="https://chit-chat-app-api-zeta.vercel.app";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setAvatar`;
